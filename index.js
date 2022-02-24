325aac63f14cea6689393814b3e0cccbd336a2ed
Autumn Loveless <autumnloveless18@gmail.com>
2022-02-23 19:21:38 -0600

    ascii changes

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 console.log(`
+                          |--__
+                          |
+                          X
+                |-___   / \\       |--__
+                |      =====      |
+                X      | .:|      X
+                / \\     | O |     / \\
+              =====   |:  . |   =====
+              |.: |__| .   : |__| :.|
+              |  :|. :  ...   : |.  |
+      __   __W| .    .  ||| .      :|W__  --
+    -- __  W  WWWW______"""______WWWW   W -----  --
+-  -     ___  ---    ____     ____----       --__  -
+    --__    --    --__     -___        __-   _
 
-                                 |--__
-                                 |
-                                 X
-                        |-___   / \\       |--__
-                        |      =====      |
-                        X      | .:|      X
-                       / \\     | O |     / \\
-                      =====   |:  . |   =====
-                      |.: |__| .   : |__| :.|
-                      |  :|. :  ...   : |.  |
-              __   __W| .    .  ||| .      :|W__  --
-            -- __  W  WWWW______"""______WWWW   W -----  --
-        -  -     ___  ---    ____     ____----       --__  -
-            --__    --    --__     -___        __-   _
+Welcome to my site! 
+If you can see this, good for you!
 
-                  Welcome to my site! 
-                  If you can see this, good for you!
+Want to collaborate on a project? Reach out!
+https://www.linkedin.com/in/autumn-loveless/
 
-                  Want to collaborate on a project? Reach out!
-                  https://www.linkedin.com/in/autumn-loveless/
+Enjoy the site,
+Autumn
 
-                  Enjoy the site,
-                  Autumn
-
-                  P.S. try the button on the top right
+P.S. try the button on the top right
 
 `)
